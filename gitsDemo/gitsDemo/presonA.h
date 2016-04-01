6b74dc9ae5a7d3a03fad31a6c2c52d26b5e654a9
qianfeng <qianfeng@womenderenshengmeiyoucaipaimeitiandoushixianchangzhibo.local>
2016-04-01 15:10:20 +0800

    创建了presonA类

---
+++ gitsDemo/gitsDemo/presonA.h
@@ -0,0 +1,13 @@
+//
+//  presonA.h
+//  gitsDemo
+//
+//  Created by qianfeng on 2016/4/1.
+//  Copyright © 2016年 YYF. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface presonA : NSObject
+
+@end
---
+++ gitsDemo/gitsDemo/presonA.m
@@ -0,0 +1,13 @@
+//
+//  presonA.m
+//  gitsDemo
+//
+//  Created by qianfeng on 2016/4/1.
+//  Copyright © 2016年 YYF. All rights reserved.
+//
+
+#import "presonA.h"
+
+@implementation presonA
+
+@end
