8dcaac9b9de6bbcc7a4ba3b527d2521841c3f59b
qianfeng <qianfeng@womenderenshengmeiyoucaipaimeitiandoushixianchangzhibo.local>
2016-04-01 14:50:39 +0800

    第一次创建项目

---
+++ gitsDemo/gitsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitsDemo
+//
+//  Created by qianfeng on 2016/4/1.
+//  Copyright © 2016年 YYF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitsDemo/gitsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitsDemo
+//
+//  Created by qianfeng on 2016/4/1.
+//  Copyright © 2016年 YYF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
